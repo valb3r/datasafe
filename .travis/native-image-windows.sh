02a86018e61590583b69f821200a675683b8ab9c
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-09-02 15:17:52 +0300

    Change path for native-image to JRE

--- .travis/native-image-windows.sh
+++ .travis/native-image-windows.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-NATIVE_IMAGE_CMD=$(echo "$JAVA_HOME/jre/bin/native-image.cmd" | sed 's!^/c/!C:\\!g' | sed 's!/!\\!g')
+NATIVE_IMAGE_CMD=$(echo "$JAVA_HOME/bin/native-image.cmd" | sed 's!^/c/!C:\\!g' | sed 's!/!\\!g')
 echo "Starting native-image from $NATIVE_IMAGE"
 
-cmd "//c $NATIVE_IMAGE_CMD -jar target\datasafe-cli-pkg.jar -H:+ReportUnsupportedElementsAtRuntime -H:Name=native-image-test --no-server --no-fallback --enable-all-security-services --features=de.adorsys.datasafe.cli.hacks.GraalCompileFixNpeOnMissingServiceTypeInKnownProviders --initialize-at-build-time=org.apache.http.conn.routing.HttpRoute --initialize-at-build-time=org.apache.http.conn.HttpClientConnectionManager --initialize-at-build-time=org.apache.http.HttpClientConnection --initialize-at-build-time=org.apache.http.conn.ConnectionRequest --initialize-at-build-time=org.apache.http.protocol.HttpContext --initialize-at-build-time=org.apache.http.pool.ConnPoolControl --initialize-at-run-time=org.bouncycastle.crypto.prng.SP800SecureRandom --initialize-at-run-time='org.bouncycastle.jcajce.provider.drbg.DRBG\$Default' --initialize-at-run-time='org.bouncycastle.jcajce.provider.drbg.DRBG\$NonceAndIV"
+cmd //c $NATIVE_IMAGE_CMD \
+    -jar datasafe-cli/target/datasafe-cli-pkg.jar \
+    -H:+ReportUnsupportedElementsAtRuntime \
+    -H:Name=native-image-test \
+    --no-fallback \
+    --enable-all-security-services \
+    --features=de.adorsys.datasafe.cli.hacks.GraalCompileFixNpeOnMissingServiceTypeInKnownProviders \
+    --initialize-at-build-time=org.apache.http.conn.routing.HttpRoute \
+    --initialize-at-build-time=org.apache.http.conn.HttpClientConnectionManager \
+    --initialize-at-build-time=org.apache.http.HttpClientConnection \
+    --initialize-at-build-time=org.apache.http.conn.ConnectionRequest \
+    --initialize-at-build-time=org.apache.http.protocol.HttpContext \
+    --initialize-at-build-time=org.apache.http.pool.ConnPoolControl \
+    --initialize-at-run-time=org.bouncycastle.crypto.prng.SP800SecureRandom \
+    --initialize-at-run-time='org.bouncycastle.jcajce.provider.drbg.DRBG$Default' \
+    --initialize-at-run-time='org.bouncycastle.jcajce.provider.drbg.DRBG$NonceAndIV'
