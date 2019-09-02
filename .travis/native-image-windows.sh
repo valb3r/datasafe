9f01f3ce2181ef560a92b7e704afd5bfebc003b9
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-09-02 18:44:05 +0300

    Fixed script path

--- .travis/native-image-windows.sh
+++ .travis/native-image-windows.sh
@@ -3,4 +3,4 @@
 NATIVE_IMAGE_CMD=$(echo "$JAVA_HOME/bin/native-image.cmd" | sed 's!^/c/!C:\\!g' | sed 's!/!\\!g')
 echo "Starting native-image from $NATIVE_IMAGE"
 
-cmd //E:ON //V:ON //T:0E //K .travis\sdk71.cmd "$NATIVE_IMAGE_CMD"
+cmd //E:ON //V:ON //T:0E //K .travis\\sdk71.cmd "$NATIVE_IMAGE_CMD"
