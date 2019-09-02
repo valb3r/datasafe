5806c04d3b183ee1b739e8de9afdbe00c0880d43
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-09-02 13:00:21 +0300

    Bundle wslpath functionality as regex

--- .travis/native-image-windows.sh
+++ .travis/native-image-windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NATIVE_IMAGE_CMD=$(echo "$JAVA_HOME\bin\native-image.cmd" | sed 's!/mnt/c/!C:\\!g' | sed 's!/!\\!g')
+NATIVE_IMAGE_CMD=$(echo "$JAVA_HOME\bin\native-image.cmd" | sed 's!^c/!C:\\!g' | sed 's!/!\\!g')
 echo "Starting native-image from $NATIVE_IMAGE_CMD"
 
 cmd.exe /C "$NATIVE_IMAGE_CMD" -jar target\datasafe-cli-pkg.jar -H:+ReportUnsupportedElementsAtRuntime -H:Name=native-image-test --no-server --no-fallback --enable-all-security-services --features=de.adorsys.datasafe.cli.hacks.GraalCompileFixNpeOnMissingServiceTypeInKnownProviders --initialize-at-build-time=org.apache.http.conn.routing.HttpRoute --initialize-at-build-time=org.apache.http.conn.HttpClientConnectionManager --initialize-at-build-time=org.apache.http.HttpClientConnection --initialize-at-build-time=org.apache.http.conn.ConnectionRequest --initialize-at-build-time=org.apache.http.protocol.HttpContext --initialize-at-build-time=org.apache.http.pool.ConnPoolControl --initialize-at-run-time=org.bouncycastle.crypto.prng.SP800SecureRandom --initialize-at-run-time='org.bouncycastle.jcajce.provider.drbg.DRBG$Default' --initialize-at-run-time='org.bouncycastle.jcajce.provider.drbg.DRBG$NonceAndIV'
