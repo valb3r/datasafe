f630d2434ce9c60f9ba43e0601b680fd88554f89
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-09-19 16:31:04 +0300

    Revert to verify

--- datasafe-storage/datasafe-storage-impl-fs/src/main/java/de/adorsys/datasafe/storage/impl/fs/FileSystemStorageService.java
+++ datasafe-storage/datasafe-storage-impl-fs/src/main/java/de/adorsys/datasafe/storage/impl/fs/FileSystemStorageService.java
@@ -55,7 +55,7 @@ public class FileSystemStorageService implements StorageService {
                         new BaseResolvedResource(
                                 // We store path in uri-encoded form, so toUri calls will fail
                                 new BasePrivateResource(
-                                        new Uri(URI.create(UriEncoderDecoder.decodeAndDropAuthority(it.toUri())))
+                                        new Uri(URI.create("file://" + it.toFile().getAbsolutePath()))
                                 ),
                                 Instant.ofEpochMilli(it.toFile().lastModified()))
                         )
